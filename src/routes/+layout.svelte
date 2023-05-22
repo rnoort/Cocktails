4216073e950e24ceb219beb6b7c9924cfdfbf8d7
Robijn Noort <rrnoort@gmail.com>
2023-05-22 22:06:11 +0200

    feat: navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<style lang="scss">
+    nav {
+        display: flex;
+        justify-content: space-between;
+        min-height: 5rem;
+        background-color: #218225;
+        padding-right: 1rem;
+        padding-left: 1rem;
+        .title {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            a {
+                font-family: sans-serif;
+                font-size: 2rem;
+                color: white;
+                text-decoration: none;
+            }
+        }
+        .links {
+            ul {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+                display: flex;
+                justify-content: space-between;
+                gap: 1rem;
+                li {
+                    display: flex;
+                    list-style-type: none;
+                    a {
+                        font-family: sans-serif;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        text-decoration: none;
+                        padding: 1rem;
+                        color: white;
+                        font-size: 1.5rem;
+                    }
+                }
+            }
+        }
+    }
+</style>
+
+<nav>
+    <div class="title">
+        <a href="/">Cocktail</a>
+    </div>
+    <div class="links">
+        <ul>
+        </ul>
+    </div>
+</nav>
+
+<slot />
